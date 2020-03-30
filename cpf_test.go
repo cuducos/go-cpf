47fe916996d56efb354acd3d79165db1c5ac3701
Eduardo Cuducos <cuducos@users.noreply.github.com>
2020-03-30 15:23:25 -0400

    Creates a Go module

--- cpf_test.go
+++ cpf_test.go
@@ -11,32 +11,32 @@ func TestMask(t *testing.T) {
 		{"123456", "123456"},
 		{"11223344556677889900", "11223344556677889900"},
 	} {
-		if got := Cpf(tc.cpf).Mask(); tc.expected != got {
-			t.Errorf("Cpf(\"%s\").Mask() = %v; want %s", tc.cpf, got, tc.expected)
+		if got := Mask(tc.cpf); tc.expected != got {
+			t.Errorf("Mask(\"%s\") = %v; expected %s", tc.cpf, got, tc.expected)
 		}
 	}
 }
 
 func TestUnmask(t *testing.T) {
-	if got := Cpf("111.111.111-11").Unmask(); "11111111111" != got {
-		t.Errorf("Cpf(\"111.111.111-11\").Unmask() = %v; want 11111111111", got)
+	if got := Unmask("111.111.111-11"); "11111111111" != got {
+		t.Errorf("Unmask(\"111.111.111-11\") = %v; want 11111111111", got)
 	}
 }
 
 func TestIsValid(t *testing.T) {
 	for _, tc := range []struct {
-		cpf      Cpf
+		cpf      string
 		expected bool
 	}{
-		{Cpf("23858488135"), true},
-		{Cpf("238.584.881-35"), true},
-		{Cpf("123"), false},
-		{Cpf("111.111.111-11"), false},
-		{Cpf("123.456.769/01"), false},
-		{Cpf("ABC.DEF.GHI-JK"), false},
+		{"23858488135", true},
+		{"238.584.881-35", true},
+		{"123", false},
+		{"111.111.111-11", false},
+		{"123.456.769/01", false},
+		{"ABC.DEF.GHI-JK", false},
 	} {
-		if got := tc.cpf.IsValid(); tc.expected != got {
-			t.Errorf("Cpf(%v).IsValid() = %v; want %v", tc.cpf, got, tc.expected)
+		if got := IsValid(tc.cpf); tc.expected != got {
+			t.Errorf("IsValid(%v) = %v; expected %v", tc.cpf, got, tc.expected)
 		}
 	}
 }
